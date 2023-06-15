2e4c6278ab8774d98b78d93aafa7635283e76f8d
femstarola <femstarola11@gmail.com>
2023-06-15 15:13:39 +0100

    initial commit

---
+++ dcrm/mydb.py
@@ -0,0 +1,21 @@
+# install mysql on your computer
+# https://dev.mysql.com/downloads/installer/
+# pip install mysql
+# pip install mysql-connector
+# pip install mysql-connector-python
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'Ijeoma1$'
+)
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# Create a database
+cursorObject.execute("CREATE DATABASE alex")
+
+print('All Done!')
